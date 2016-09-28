47b82afd738283981a3ebeffb868d125a672de42
阿朋 <913305330@qq.com>
2016-09-29 00:56:21 +0800

    初始化项目

---
+++ apeng123/apeng123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  apeng123
+//
+//  Created by 阿朋 on 16/9/29.
+//  Copyright © 2016年 apeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ apeng123/apeng123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  apeng123
+//
+//  Created by 阿朋 on 16/9/29.
+//  Copyright © 2016年 apeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
